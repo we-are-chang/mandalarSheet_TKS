27f47121b4c2eba7b171d8c49b968198f5c0ae94
we-are-chang <sato_ami@ca-techkids.com>
2026-05-19 16:59:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -49,12 +49,12 @@ const centerTemplates = {
     ],
     [
       { placeholder: "作品のどんなところが好き？" },
-      { placeholder: "作品のどんなところが好き？" },
+      { placeholder: "ここに、好きな作品を書いてみよう" },
       { placeholder: "作品のどんなところが好き？" },
     ],
     [
       { placeholder: "作品のどんなところが好き？" },
-      { placeholder: "ここに、好きな作品を書いてみよう" },
+      { placeholder: "作品のどんなところが好き？" },
       { placeholder: "作品のどんなところが好き？" },
     ],
   ],
